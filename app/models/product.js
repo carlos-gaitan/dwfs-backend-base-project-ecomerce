df744ed4d141786137267b3b0ef244ceba2d1b7d
Juan Manuel <juanmanuel.tartaglia@gmail.com>
2018-11-02 20:17:28 -0300

    Test completos Modelo Productos.add

---
+++ app/models/product.js
@@ -0,0 +1,46 @@
+var connection = require('../../database')
+
+var Product = function(id ,name ,description ,price ,oldPrice ,image ,active) {
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.price = price;
+  this.oldPrice = oldPrice;
+  this.image = image;
+  this.active = active;  
+}
+
+Product.getAll = function (cb) {
+  connection.query('SELECT * FROM products', cb)
+}
+
+Product.add = function(data ,cb){
+  if (data != null && data.name != null && data.description != null && data.image != null) {
+    var sqlQuery = `INSERT INTO products (name,description,price,oldPrice,image,active)
+      VALUES ('${data.name}','${data.description}',${data.price},${data.oldPrice},'${data.image}',1)`;
+    connection.query(sqlQuery, function(err,res){
+      var resultado;
+      var error;
+      if(!err){
+        //console.log(res)
+        resultado = { succes: true, msg: "Producto agregado OK", id: res.insertId};
+      }
+      else {
+        error = { succes: false, msg: "Error al insertar un producto"};
+      }
+      cb(error,resultado);
+    });
+  }
+  else {
+    var error = { succes: false, msg: "Error al insertar un producto"};
+    cb(error,null);
+  }
+}
+/*
+User.add = function (data, cb) {
+  var sqlQuery = `INSERT INTO products (name, email, password, active)
+    VALUES ('${data.name}','${data.email}','${data.password}',1)`;
+  connection.query(sqlQuery, cb)
+}
+*/
+module.exports = Product
---
+++ test/models/product.js
@@ -0,0 +1,141 @@
+var expect = require('chai').expect
+var ProductModel = require('../../app/models/product')
+var utils = require('../utils')
+
+describe('Product Model', function() {
+  before(function(done) {
+    this.timeout(10000);
+    utils.cleanDB(function(){
+      done();
+    })   
+  })
+
+  describe('#add',function(){
+    it('add debe ser una funcion',function(done){
+      expect(ProductModel.add).to.be.a('function');
+      done();
+    });
+    it('Agrega un producto con sus datos correctos', function(done) {
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: "TV 14", description: "Super tv de 14 pulgadas alta definicion", price: 1000, oldPrice: 1500, image: "none"}, function(err, res){
+        expect(err).not.to.exist;
+        expect(res.succes).to.be.true;
+        expect(res.msg).to.be.a('string');
+        expect(res.id).to.be.a('number');
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad + 1);
+            done();
+          }
+        });
+      });
+    });
+    // it begin
+    it('No debe agregar si "name" es nulo',function(done){
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: null, description: "Super tv de 14", price: 1000, oldPrice: 1500, image: "none"}, function(err, res){
+        expect(err).to.exist;
+        expect(err.succes).to.be.false;
+        //expect(res).not.to.be.exist;
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad);
+            done();
+          }
+        });
+      });
+    });
+    // end it
+
+    it('No debe agregar si "description" es nulo',function(done){
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: "TV 14 Pulgadas", description: null, price: 1000, oldPrice: 1500, image: "none"}, function(err, res){
+        expect(err).to.exist;
+        expect(err.succes).to.be.false;
+        expect(res).not.to.be.exist;
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad);
+            done();
+          }
+        });
+      });
+    });
+
+    it('No debe agregar si "price" es nulo',function(done){
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: "TV 14 pulgadas", description: "Super tv de 14", price: null, oldPrice: 1500, image: "none"}, function(err, res){
+        expect(err).to.exist;
+        expect(err.succes).to.be.false;
+        expect(res).not.to.be.exist;
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad);
+            done();
+          }
+        });
+      });
+    });
+
+    it('No debe agregar si "oldPrice" es nulo',function(done){
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: "TV 14 Pulgadas", description: "Super tv de 14 high def", price: 1000, oldPrice: null, image: "none"}, function(err, res){
+        expect(err).to.exist;
+        expect(err.succes).to.be.false;
+        expect(res).not.to.be.exist;
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad);
+            done();
+          }
+        });
+      });
+    });
+
+    it('No debe agregar si "imgage" es nulo',function(done){
+      var cantidad = null;
+      ProductModel.getAll(function (err,res) {
+        if(!err) {
+          cantidad = res.length;
+        }
+      });
+      ProductModel.add({name: "TV 14 Pulgadas", description: "Super tv de 14", price: 1000, oldPrice: 1500, image: null}, function(err, res){
+        expect(err).to.exist;
+        expect(err.succes).to.be.false;
+        expect(res).not.to.be.exist;
+        ProductModel.getAll(function (err,res) {
+          if(!err) {
+            expect(res.length).to.be.equal(cantidad);
+            done();
+          }
+        });
+      });
+    });
+
+  });
+});
