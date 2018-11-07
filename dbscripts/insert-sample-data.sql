3f78c4c3871f80041e134304751a01a1d100daf0
Juan Manuel <juanmanuel.tartaglia@gmail.com>
2018-11-06 21:22:48 -0300

    script sql

---
+++ dbscripts/insert-sample-data.sql
@@ -0,0 +1,2 @@
+USE `ecomerce_test`;
+INSERT INTO `products` (name, description, price, oldPrice, image, active) VALUES ("Sample Product 1", "Sample Product ID 1", 100, 120, "none", 1);
---
+++ dbscripts/truncate-all-tables.sql
@@ -0,0 +1,11 @@
+USE `ecomerce_test`;
+SET FOREIGN_KEY_CHECKS=0;
+TRUNCATE TABLE `categories`;
+TRUNCATE TABLE `orders`;
+TRUNCATE TABLE `products`;
+TRUNCATE TABLE `productsbycategories`;
+TRUNCATE TABLE `productsbyorders`;
+TRUNCATE TABLE `users`;
+TRUNCATE TABLE `wishlist`;
+SET FOREIGN_KEY_CHECKS=1;
+
